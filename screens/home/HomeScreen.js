29b078756efc2c0736f8fdc5f09300454a36400e
bhanu <bhanu.yandra@gmail.com>
2019-05-31 11:34:50 +0100

    Initial commit

---
+++ screens/home/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Body, Container, Content, Header, Text} from "native-base";
+
+export const HomeScreen = () => {
+    return <Container>
+        <Header><Body><Text>Street views</Text></Body></Header>
+        <Content>
+            <Text>Account</Text>
+        </Content>
+
+    </Container>
+};
+export default HomeScreen;
